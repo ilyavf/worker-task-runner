ab82b1e8cae50d76b11d637ff2dab1db0d7df723
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-05-04 17:36:27 -0600

    updated readme with SharedArrayBuffer

--- demo/shared-array-task/demo.js
+++ demo/shared-array-task/demo.js
@@ -2,7 +2,7 @@ const { create, runTask, createBuffer, bufferToArray } = require('../../src/help
 const execTask = require('../../src/utils/exec')
 
 // Create shared array buffer to send to worker:
-const data = { buffer: createBuffer(50, window.Int32Array), type: 'Int32Array'}
+const data = { buffer: createBuffer(50, 'Int32Array'), type: 'Int32Array'}
 
 // Define the main app: create a worker and run a task:
 const app =
